e9458ea9e665925b6f424299f78e037c998ab652
GagandeepV <gagandeep.veerabhadregowda@meredith.com>
2022-08-14 04:21:23 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Speech from 'react-speech'
+import style from './styles.json'
+import './index.css'
 
 function App() {
+  const [Text, setText] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Text 2 Speech</h1>
+      <div className="container">
+        <textarea rows="4" cols="90" placeholder='Here....'
+          value={Text} onChange={({ target: { value } }) => setText(value)}
+        />
+        <Speech
+          styles={style}
+          stop={true}
+          pause={true}
+          resume={true}
+          text={Text}
+        />
+      </div>
+    </>
+  )
 }
 
 export default App;
